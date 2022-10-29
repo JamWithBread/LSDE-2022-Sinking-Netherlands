5cb6011f61dd8ee7e7a59963345a7bd7cb287846
hkstm <kailhan@hkstm.dev>
2022-10-29 16:17:10 +0200

    Reflect state of application more clearly

--- frontend/src/three/Toolbar.tsx
+++ frontend/src/three/Toolbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import metadata_ahn3 from "../chunks/ahn3/_metadata.json";
+
 type ToolbarProps = {
     isRenderingMeshAHN3: boolean,
     toggleRenderingMeshAHN3: () => void,
@@ -9,25 +11,32 @@ type ToolbarProps = {
     waterHeight: number,
     setWaterHeight: (arg0: number) => void,
 }
+const minY = metadata_ahn3.minY
+const maxY = metadata_ahn3.maxY
+const yRange = maxY - minY
 
 function Toolbar(props: ToolbarProps) {
     function handleSliderChange(e: React.ChangeEvent<HTMLInputElement>) {
         props.setWaterHeight((parseFloat(e.target.value)))
     }
+
     return (
         <div>
             <div>
-                <button onClick={props.toggleRenderingMeshAHN3}>
+                <button style={{opacity: (props.isRenderingMeshAHN3) ? 1 : 0.5}}
+                        onClick={props.toggleRenderingMeshAHN3}>
                     Toggle Mesh AHN3
                 </button>
             </div>
             <div>
-                <button onClick={props.toggleRenderingMeshAHN2}>
+                <button style={{opacity: (props.isRenderingMeshAHN2) ? 1 : 0.5}}
+                        onClick={props.toggleRenderingMeshAHN2}>
                     Toggle Mesh AHN2
                 </button>
             </div>
             <div>
-                <button onClick={props.toggleRenderingWater}>
+                <button style={{opacity: (props.isRenderingWater) ? 1 : 0.5}}
+                        onClick={props.toggleRenderingWater}>
                     Toggle Water
                 </button>
             </div>
@@ -35,6 +44,8 @@ function Toolbar(props: ToolbarProps) {
                 <input type="range" min="0" max="100" className="slider"
                        value={props.waterHeight}
                        onChange={handleSliderChange}/>
+                <p> Current water
+                    level: {props.isRenderingWater ? (((props.waterHeight / 100) - 0.5) * (yRange)).toFixed(2) : "-"}</p>
             </div>
             <div>
                 <p><code>W</code> Up</p>
